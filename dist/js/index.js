aac87ebf862f2cb408e131eb507e489b15e6b2f2
Greg <g.yaniw@gmail.com>
2019-03-31 15:12:39 -0400

    added function to create text input

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,12 +1,45 @@
 "use strict";
 ;
+var createInputElement = function (inputValue) {
+    var inputElement = document.createElement('input');
+    inputElement.type = 'text';
+    inputElement.value = inputValue;
+    inputElement.setAttribute('style', 'display: none');
+    return inputElement;
+};
+var UpdateButton = /** @class */ (function () {
+    function UpdateButton(button) {
+        this.button = button;
+    }
+    UpdateButton.prototype.getUpdateButton = function () {
+        var updateButton = document.createElement('button');
+        updateButton.innerText = this.button;
+        updateButton.className = 'update-button';
+        updateButton.setAttribute('style', 'display: none');
+        return updateButton;
+    };
+    return UpdateButton;
+}());
+var EditButton = /** @class */ (function () {
+    function EditButton(button) {
+        this.button = button;
+    }
+    EditButton.prototype.getEditButton = function () {
+        var editButton = document.createElement('button');
+        editButton.innerText = this.button;
+        editButton.className = 'edit-button';
+        return editButton;
+    };
+    return EditButton;
+}());
 var DeleteButton = /** @class */ (function () {
     function DeleteButton(button) {
         this.button = button;
     }
-    DeleteButton.prototype.getButton = function () {
+    DeleteButton.prototype.getDeleteButton = function () {
         var deleteButton = document.createElement('button');
         deleteButton.innerText = this.button;
+        deleteButton.className = 'delete-button';
         return deleteButton;
     };
     return DeleteButton;
@@ -24,21 +57,40 @@ var clearInputAfterSubmit = function (form) {
 };
 var postTodo = function (inputData) {
     var listElement = document.createElement('li');
-    listElement.innerText = inputData.title;
+    var spanElement = document.createElement('span');
+    var spantext = document.createTextNode(inputData.title);
+    spanElement.appendChild(spantext);
+    listElement.appendChild(spanElement);
     var todoList = document.getElementById('todo-list');
+    var updateButton = new UpdateButton('Update');
+    var editButton = new EditButton('Edit');
     var deleteButton = new DeleteButton('X');
+    var inputElement = createInputElement(inputData.title);
     if (todoList !== null) {
         todoList.appendChild(listElement);
-        listElement.appendChild(deleteButton.getButton());
-        listElement.querySelector('button').addEventListener('click', function () {
+        listElement.appendChild(inputElement);
+        listElement.appendChild(updateButton.getUpdateButton());
+        listElement.appendChild(editButton.getEditButton());
+        listElement.appendChild(deleteButton.getDeleteButton());
+        var selectUpdateButton_1 = listElement.querySelector('button.update-button');
+        var selectEditButton_1 = listElement.querySelector('button.edit-button');
+        var selectDeleteButton = listElement.querySelector('button.delete-button');
+        var selectSpanElement_1 = listElement.querySelector('span');
+        selectUpdateButton_1.addEventListener('click', function () {
+        });
+        selectEditButton_1.addEventListener('click', function () {
+            listElement.insertBefore(inputElement, listElement.childNodes[0]);
+            selectSpanElement_1.setAttribute('style', 'display: none');
+            selectEditButton_1.setAttribute('style', 'display: none');
+            selectUpdateButton_1.setAttribute('style', 'display: inline-block');
+        });
+        selectDeleteButton.addEventListener('click', function () {
             listElement.parentNode.removeChild(listElement);
         });
     }
 };
 window.addEventListener('DOMContentLoaded', function () {
-    console.log('content loaded');
     var form = document.getElementById('todo-form');
-    console.log('form', form);
     if (form !== null) {
         form.addEventListener('submit', function (event) {
             event.preventDefault();
@@ -47,5 +99,4 @@ window.addEventListener('DOMContentLoaded', function () {
             postTodo(inputData);
         });
     }
-    //let deleteTodo = document.ge
 });
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,50 @@ interface Todo {
 	title: string;
 };
 
-class DeleteButton {
-	button: string;
+let createInputElement = (inputValue: string): HTMLInputElement => {
+	let inputElement = document.createElement('input');
+	inputElement.type = 'text';
+	inputElement.value = inputValue;
+	inputElement.setAttribute('style', 'display: none');
+	return inputElement;
+}
 
-	constructor(button: string, ) {
+class UpdateButton {
+	button: string;
+	constructor(button: string) {
 		this.button = button;
 	}
+	getUpdateButton(): HTMLButtonElement {
+		let updateButton = document.createElement('button');
+		updateButton.innerText = this.button;
+		updateButton.className = 'update-button';
+		updateButton.setAttribute('style', 'display: none');
+		return updateButton;
+	}
+}
 
-	getButton(): HTMLButtonElement {
+class EditButton {
+	button: string;
+	constructor(button: string) {
+		this.button = button;
+	}
+	getEditButton(): HTMLButtonElement {
+		let editButton = document.createElement('button');
+		editButton.innerText = this.button;
+		editButton.className = 'edit-button';
+		return editButton;
+	}
+}
+
+class DeleteButton {
+	button: string;
+	constructor(button: string) {
+		this.button = button;
+	}
+	getDeleteButton(): HTMLButtonElement {
 		let deleteButton = document.createElement('button');
 		deleteButton.innerText = this.button;
+		deleteButton.className = 'delete-button';
 		return deleteButton;
 	}
 }
@@ -33,25 +67,52 @@ let clearInputAfterSubmit = (form: HTMLElement) => {
 
 let postTodo = (inputData: Todo) => {
 	let listElement = document.createElement('li');
-	listElement.innerText = inputData.title;
-	let todoList = document.getElementById('todo-list');
+	let spanElement = document.createElement('span');
+	let spantext = document.createTextNode(inputData.title);
+	spanElement.appendChild(spantext);
+	listElement.appendChild(spanElement);
 
+	let todoList = document.getElementById('todo-list');
+	let updateButton = new UpdateButton('Update');
+	let editButton = new EditButton('Edit');
 	let deleteButton = new DeleteButton('X');
+	let inputElement: HTMLInputElement = createInputElement(inputData.title);
 
 	if (todoList !== null) {
 		todoList.appendChild(listElement);
-		listElement.appendChild(deleteButton.getButton());
-		listElement.querySelector('button').addEventListener('click', () => {
+		listElement.appendChild(inputElement);
+		listElement.appendChild(updateButton.getUpdateButton());
+		listElement.appendChild(editButton.getEditButton());
+		listElement.appendChild(deleteButton.getDeleteButton());
+
+
+		const selectUpdateButton: HTMLButtonElement = listElement.querySelector('button.update-button');
+		const selectEditButton: HTMLButtonElement = listElement.querySelector('button.edit-button');
+		const selectDeleteButton: HTMLButtonElement = listElement.querySelector('button.delete-button');
+		const selectSpanElement: HTMLSpanElement = listElement.querySelector('span');
+
+		selectUpdateButton.addEventListener('click', () => {
+			
+
+		});
+
+		selectEditButton.addEventListener('click', () => {
+			
+			listElement.insertBefore(inputElement, listElement.childNodes[0]);
+			selectSpanElement.setAttribute('style', 'display: none');
+
+			selectEditButton.setAttribute('style', 'display: none');
+			selectUpdateButton.setAttribute('style', 'display: inline-block');
+		});
+		
+		selectDeleteButton.addEventListener('click', () => {
 			listElement.parentNode.removeChild(<HTMLElement>listElement);
 		});
 	}
 };
 
 window.addEventListener('DOMContentLoaded', () => {
-	console.log('content loaded');
-
 	let form: HTMLElement|null = document.getElementById('todo-form');
-	console.log('form', form);
 
 	if (form !== null) {
 		form.addEventListener('submit', (event) => {
@@ -61,6 +122,4 @@ window.addEventListener('DOMContentLoaded', () => {
 			postTodo(inputData);
 		});
 	}
-
-	//let deleteTodo = document.ge
 });
